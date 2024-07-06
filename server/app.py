46aa6a323c73f17a100f415c255cd6d70effc39e
geraldkisilu <muligerald6@gmail.com>
2024-07-07 00:34:41 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -1,21 +1,70 @@
-from flask import Flask
-from flask_migrate import Migrate
-
-from models import db
+from flask import Flask, request, jsonify
+from flask_migrate import Migrate # type: ignore
+from flask_cors import CORS  # type: ignore
+from models import db, Customer, Item, Message, Review
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
+CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
-
 
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
 
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.all()
+    return jsonify([message.to_dict() for message in messages])
+
+@app.route('/messages/<int:id>', methods=['GET'])
+def get_message_by_id(id):
+    message = Message.query.get_or_404(id)
+    return jsonify(message.to_dict())
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    data = request.get_json()
+    message = Message.query.get_or_404(id)
+    if 'body' in data:
+        message.body = data['body']
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
+
+@app.route('/create_review', methods=['POST'])
+def create_review():
+    data = request.get_json()
+    new_review = Review(
+        comment=data['comment'],
+        customer_id=data['customer_id'],
+        item_id=data['item_id']
+    )
+    db.session.add(new_review)
+    db.session.commit()
+    return jsonify(new_review.to_dict()), 201
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
